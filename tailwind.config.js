f1f665cbb29fbe341950b9856088ff3d316be950
Gautam Singla <ggsingla777@gmail.com>
2024-06-03 18:16:13 +0530

    Tailwind setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./app/pages/**/*.{js,ts,jsx,tsx}",
-  "./app/components/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
